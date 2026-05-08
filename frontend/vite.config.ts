b50625edaabedc95f025e3e1b2324610c93559d5
PerryM123 <perrymiranda1@yahoo.com>
2026-05-08 20:43:36 +0900

    When using env variables to be used with vite (example: VITE_FOOBAR), must use loadEnv instead

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,25 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
-  server: {
-    host: "0.0.0.0",
-    hmr: {
-      clientPort: process.env.HMR_CLIENT_PORT
-        ? parseInt(process.env.HMR_CLIENT_PORT)
-        : undefined,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [tailwindcss(), reactRouter()],
+    resolve: {
+      tsconfigPaths: true,
     },
-    allowedHosts: process.env.VITE_ALLOWED_HOST
-      ? [process.env.VITE_ALLOWED_HOST]
-      : ["local.super-price-check.com"]
-  },
+    server: {
+      host: "0.0.0.0",
+      hmr: {
+        clientPort: process.env.HMR_CLIENT_PORT
+          ? parseInt(process.env.HMR_CLIENT_PORT)
+          : undefined,
+      },
+      allowedHosts: env.VITE_ALLOWED_HOST
+        ? [env.VITE_ALLOWED_HOST]
+        : ["local.super-price-check.com"],
+    },
+  };
 });
