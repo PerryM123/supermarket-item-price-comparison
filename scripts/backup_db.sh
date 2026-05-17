bcdcc5895a44e1b5f02a5277e8681c8c3edae94f
PerryM123 <perrymiranda1@yahoo.com>
2026-05-17 19:09:49 +0900

    Made sure the directory exists, even on fresh install to VPS

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 DB="app"
 USER="app"
-BACKUP="./db_backup/backup_$(date +%Y%m%d_%H%M%S).sql.gz"
+DB_BACKUP_DIRECTORY="db_backup"
+BACKUP="./${DB_BACKUP_DIRECTORY}/backup_$(date +%Y%m%d_%H%M%S).sql.gz"
 CONTAINER="supermarket-item-price-comparison-postgres-1"
+
+mkdir -p ./$DB_BACKUP_DIRECTORY
 
 docker exec -t supermarket-item-price-comparison-postgres-1 pg_dump -U app -d app | gzip > $BACKUP
 echo "Backup created: $BACKUP"
-
